ffd20c170cb951a03f8f97573b786b7c7e88a1ba
hungpv <hungpv@rabiloo.com>
2023-05-27 11:01:52 +0700

    add: architect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SharedModule } from './shared/shared.module';
+
+import { ConfigurationService } from './shared/configuration/configuration.service';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    SharedModule,
+    // MongooseModule.forRootAsync({
+    //   useFactory: async (configService: ConfigurationService) => ({
+    //     uri: configService.mongoUri,
+    //     useNewUrlParser: true,
+    //   }),
+    //   inject: [ConfigurationService],
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+  static isDev: boolean;
+
+  constructor(private readonly _configurationService: ConfigurationService) {
+    AppModule.port = AppModule.normalizePort(_configurationService.port);
+    AppModule.isDev = _configurationService.isDevelopment;
+  }
+
+  /**
+   * Normalize port or return an error if port is not valid
+   * @param val The port to normalize
+   */
+  private static normalizePort(val: number | string): number | string {
+    const port: number = typeof val === 'string' ? parseInt(val, 10) : val;
+
+    if (Number.isNaN(port)) {
+      return val;
+    }
+
+    if (port >= 0) {
+      return port;
+    }
+
+    throw new Error(`Port "${val}" is invalid.`);
+  }
+}
